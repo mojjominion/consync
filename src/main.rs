57077e46b03ca605a1930ccad9d8c39063ed9c33
Mohan <mohan@stackbox.xyz>
2023-07-04 00:23:37 +0530

    feat (Daemon): :sparkles: add daemonization

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
+use std::fs::File;
+
+use daemonize::Daemonize;
+
 use crate::watcher::async_watch;
 pub mod callbacks;
 pub mod filters;
 pub mod watcher;
 
-/// Async, futures channel based event watching
-fn main() {
+fn run() {
     let path = dirs::config_dir().unwrap_or_default();
     println!("watching {}", path.to_string_lossy());
 
@@ -13,4 +16,25 @@ fn main() {
             println!("error: {:?}", e)
         }
     });
+}
+
+/// Async, futures channel based event watching
+fn main() {
+    let stdout = File::create("/tmp/consync-daemon.out").unwrap();
+    let stderr = File::create("/tmp/consync-daemon.err").unwrap();
+
+    let daemonize = Daemonize::new()
+        .pid_file("/tmp/consync.pid") // Every method except `new` and `start`
+        .working_directory("/tmp") // for default behaviour.
+        .user("nobody")
+        .group("daemon")
+        .group(2)
+        .stdout(stdout) // Redirect stdout to `/tmp/daemon.out`.
+        .stderr(stderr) // Redirect stderr to `/tmp/daemon.err`.
+        .privileged_action(|| run());
+
+    match daemonize.start() {
+        Ok(_) => println!("Success, daemonized"),
+        Err(e) => eprintln!("Error, {}", e),
+    };
 }
